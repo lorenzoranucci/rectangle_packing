884ae57457a1dad55e445e92f455020922f44317
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2017-05-08 16:43:07 +0200

    completed

--- subfunctions/pointsBetweenTwoPoints.pl
+++ subfunctions/pointsBetweenTwoPoints.pl
@@ -1,5 +1,8 @@
 %[pointsBetweenTwoPoints].    
 %pointsBetweenTwoPoints(point(10,13), point(15,13), Result).
+%pointsBetweenTwoPoints(point(10,13), point(10,16), Result).
+
+
 
 pointsBetweenTwoPoints(point(X0,Y0), point(X0,Y0), [point(X0,Y0)|Result]).
 
@@ -8,5 +11,13 @@ pointsBetweenTwoPoints(point(X0,Y0), point(X1,Y0), [point(X0,Y0)|Result]):-
 	NEW_X0 #= X0+1,
 	pointsBetweenTwoPoints(point(NEW_X0,Y0), point(X1,Y0), Result)
 .
+
+
+pointsBetweenTwoPoints(point(X0,Y0), point(X0,Y1), [point(X0,Y0)|Result]):-	
+	Y0#<Y1,	
+	NEW_Y0 #= Y0+1,
+	pointsBetweenTwoPoints(point(X0,NEW_Y0), point(X0,Y1), Result)
+.
+
 
 
