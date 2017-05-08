5f0883262220822dc5c8bdd24db753aca451531d
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2017-05-08 19:15:16 +0200

    completed with duplication

--- subfunctions/getPerimeterPoints.pl
+++ subfunctions/getPerimeterPoints.pl
@@ -1,18 +1,19 @@
 %[getPerimeterPoints].
 %getPerimeterPoints(rect(R,vertex(0,5,0),vertex(1,7,0),vertex(2,7,2),vertex(3,5,2), 2, 2)  ,  Result).
 %getAllPerimetersPoints([rect(R,vertex(0,5,0),vertex(1,7,0),vertex(2,7,2),vertex(3,5,2), 2, 2)]  ,  Result).
-%getAllPerimetersPoints([rect(R,vertex(0,5,0),vertex(1,7,0),vertex(2,7,2),vertex(3,5,2), 2, 2),rect(R,vertex(0,5,0),vertex(1,8,0),vertex(2,8,2),vertex(3,5,2), 2, 2)]  ,  Result).
+%getAllPerimetersPoints([rect(R,vertex(0,5,0),vertex(1,7,0),vertex(2,7,2),vertex(3,5,2), 2, 2),rect(R2,vertex(0,5,0),vertex(1,8,0),vertex(2,8,2),vertex(3,5,2), 2, 2)]  ,  Result).
+%getAllPerimetersPoints([rect(R,vertex(0,5,0),vertex(1,7,0),vertex(2,7,2),vertex(3,5,2), 2, 2),rect(R2,vertex(0,5,0),vertex(1,8,0),vertex(2,8,2),vertex(3,5,2), 3, 2),rect(R3,vertex(0,5,0),vertex(1,8,0),vertex(2,8,2),vertex(3,5,2), 3, 2)]  ,  Result).
 
 
 
 getAllPerimetersPoints([],[]).
 getAllPerimetersPoints(
 			[rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0,Y1), B, H) | Rest],
-			[Result|SubResult]
+			Result
 			
-):-	getPerimeterPoints(rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0,Y1), B, H)  ,  Result),
-	getAllPerimetersPoints(Rest,SubResult)
-	
+):-	getPerimeterPoints(rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0,Y1), B, H)  ,  SubResult1),
+	getAllPerimetersPoints(Rest,SubResult2),
+	concat(SubResult1, SubResult2, Result)
 .
 	
 
@@ -49,4 +50,5 @@ pointsBetweenTwoPoints(point(X0,Y0), point(X0,Y1), [point(X0,Y0)|Result]):-
 concat([],[],[]).
 
 concat([],[X2|L2], [X2|Result]):- concat([],L2,Result).
+concat([X1|L1],[], [X1|Result]):- concat(L1,[],Result).
 concat([X1|L1],[X2|L2], [X1|Result]):- concat(L1,[X2|L2],Result).
