49b86d550f1ec8c38b4ef1e3c2c06a83452f82b9
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2017-05-16 19:40:16 +0200

    Introduce sources including

--- subfunctions/getAreaPoints.pl
+++ subfunctions/getAreaPoints.pl
@@ -28,19 +28,5 @@ getAreaPoints(rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0
 	
 	
 
-pointsBetweenTwoPoints(point(X0,Y0), point(X0,Y0), [point(X0,Y0)]).
-
-pointsBetweenTwoPoints(point(X0,Y0), point(X1,Y0), [point(X0,Y0)|Result]):-	
-	X0#<X1,	
-	NEW_X0 #= X0+1,
-	pointsBetweenTwoPoints(point(NEW_X0,Y0), point(X1,Y0), Result)
-.
-
-
-pointsBetweenTwoPoints(point(X0,Y0), point(X0,Y1), [point(X0,Y0)|Result]):-	
-	Y0#<Y1,	
-	NEW_Y0 #= Y0+1,
-	pointsBetweenTwoPoints(point(X0,NEW_Y0), point(X0,Y1), Result)
-.
-
+:-include('pointsBetweenTwoPoints.pl').
 
