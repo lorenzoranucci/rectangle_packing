14a2a000ec2c4f4ee027334f8d954fa41ff517c1
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2017-05-18 22:26:52 +0200

    Fix bug duplicated points

--- subfunctions/getPerimeterPoints.pl
+++ subfunctions/getPerimeterPoints.pl
@@ -4,7 +4,8 @@
 %getAllPerimetersPoints([rect(R,vertex(0,5,0),vertex(1,7,0),vertex(2,7,2),vertex(3,5,2), 2, 2),rect(R2,vertex(0,5,0),vertex(1,8,0),vertex(2,8,2),vertex(3,5,2), 2, 2)]  ,  Result).
 %getAllPerimetersPoints([rect(R,vertex(0,5,0),vertex(1,7,0),vertex(2,7,2),vertex(3,5,2), 2, 2),rect(R2,vertex(0,5,0),vertex(1,8,0),vertex(2,8,2),vertex(3,5,2), 3, 2),rect(R3,vertex(0,5,0),vertex(1,8,0),vertex(2,8,2),vertex(3,5,2), 3, 2)]  ,  Result).
 
-
+:-include('removeDuplicates.pl').
+:-include('pointsBetweenTwoPoints.pl').
 
 
 getAllPerimetersPoints([],[]).
@@ -28,11 +29,11 @@ getPerimeterPoints(rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex
 	pointsBetweenTwoPoints(point(X1,Y0), point(X1,Y1), Result4),
 	append(Result1,Result2,Res1),
 	append(Result3,Result4,Res2),
-	append(Res1,Res2,Result)	
+	append(Res1,Res2,Res3),
+	removeDuplicates(Res3,Result,_)	
 .
 	
 
 
-:-include('pointsBetweenTwoPoints.pl').
 
 
