bd49548c59bb757aaabfa90453eadb5dd3835381
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2017-08-01 17:38:36 +0200

    add comments

--- subfunctions/getAreaPoints.pl
+++ subfunctions/getAreaPoints.pl
@@ -5,7 +5,10 @@
 
 %getAllAreaPoints([rect(5,vertex(0,0,1),vertex(1,3,1),vertex(2,3,3),vertex(3,0,3), 3, 2), rect(6,vertex(0,5,5),vertex(1,7,5),vertex(2,7,7),vertex(3,5,7), 2, 2)]  ,  Result).
 
+:-include('pointsBetweenTwoPoints.pl').
 
+
+%Get all input list's rects area's points 
 getAllAreaPoints([],[]).
 getAllAreaPoints([rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0,Y1), B, H)|Tail],Result)
 :-
@@ -16,6 +19,7 @@ getAllAreaPoints([rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(
 
 getAreaPoints(rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0,Y1), B, H)  ,  []):-Y0#>=Y1.
 
+%Get single rect area's points list
 getAreaPoints(rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0,Y1), B, H)  ,  Result)
 :-	
 	NEWX1#=X1-1,
@@ -28,5 +32,5 @@ getAreaPoints(rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0
 	
 	
 
-:-include('pointsBetweenTwoPoints.pl').
+
 
