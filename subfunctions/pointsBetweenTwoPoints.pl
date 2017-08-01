865863f14eb278cfbdf6ceff4ef59a85b9b77f2a
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2017-08-01 16:03:28 +0200

    let works also with X0 greater then X1

--- subfunctions/pointsBetweenTwoPoints.pl
+++ subfunctions/pointsBetweenTwoPoints.pl
@@ -1,15 +1,23 @@
 %[pointsBetweenTwoPoints].    
 %pointsBetweenTwoPoints(point(10,13), point(15,13), Result).
+%pointsBetweenTwoPoints(point(15,13), point(10,13), Result).
 %pointsBetweenTwoPoints(point(10,13), point(10,16), Result).
+%pointsBetweenTwoPoints(point(10,16), point(10,13), Result).
 
 
-
+%Get all segment's points
 pointsBetweenTwoPoints(point(X0,Y0), point(X0,Y0), [point(X0,Y0)|[]]).
 
 pointsBetweenTwoPoints(point(X0,Y0), point(X1,Y0), [point(X0,Y0)|Result]):-	
 	X0#<X1,	
 	NEW_X0 #= X0+1,
 	pointsBetweenTwoPoints(point(NEW_X0,Y0), point(X1,Y0), Result)
+.
+
+pointsBetweenTwoPoints(point(X0,Y0), point(X1,Y0), [point(X1,Y0)|Result]):-	
+	X0#>X1,	
+	NEW_X1 #= X1+1,
+	pointsBetweenTwoPoints(point(X0,Y0), point(NEW_X1,Y0), Result)
 .
 
 
@@ -19,5 +27,11 @@ pointsBetweenTwoPoints(point(X0,Y0), point(X0,Y1), [point(X0,Y0)|Result]):-
 	pointsBetweenTwoPoints(point(X0,NEW_Y0), point(X0,Y1), Result)
 .
 
+
+pointsBetweenTwoPoints(point(X0,Y0), point(X0,Y1), [point(X0,Y1)|Result]):-	
+	Y0#>Y1,	
+	NEW_Y1 #= Y1+1,
+	pointsBetweenTwoPoints(point(X0,Y0), point(X0,NEW_Y1), Result)
+.
 
 
