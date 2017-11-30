27a394b9a00b28e0dcff392f45b15d1e92e157a5
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2017-11-30 19:37:40 +0100

    add comments

--- prolog/solution.pl
+++ prolog/solution.pl
@@ -16,12 +16,22 @@
 
 
 
-
+%Given a list of rectangles returns the same list with rectangles coordinates
+%	Rect	list of rect to locate (with variable coordinates)
+%	Result	list of located rectangles
 solution(Rect,Result):-solutionAux(Rect,[],Result).
+
+
+%Given a list of rectangles and a list of already positioned rectangles returns the previous list of positioned rectangles improved with the head of first list
+%	1			list of rectangles to locate (with variable coordinates)
+%	PositionedRects		list of rectangles already located
+%	Result			list of located rectangles
 solutionAux([], PositionedRects,PositionedRects).
 solutionAux([rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0,Y1), B, H)|Positionings], PositionedRects,Result)
 :-
+	%greedily choice the best head rect position depending on already positioned rectangles
 	getBestPositioning(rect(R,vertex(0,X0,Y0),vertex(1,X1,Y0),vertex(2,X1,Y1),vertex(3,X0,Y1), B, H),PositionedRects,SubResult1),
+	%remove head from the list of rectangles to locate and add it (with coordinates) to positioned rects	
 	solutionAux(Positionings, SubResult1,Result)
 .
 
